ba2d1651754c2a4cf9327688650a0337c7810cc4
Ratnakumar <Ratnakumar@WSHYD-021.WILCO.COM>
2020-02-27 19:47:55 +0530

    new projects

---
+++ SamplePrograms/src/myPackage/PrintNumbers.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=0; i<=100; i++)
+		{
+			if(i%5==0)
+			{
+			System.out.println("the  numbers are "+ i);
+			}
+		}
+
+	}
+
+}
