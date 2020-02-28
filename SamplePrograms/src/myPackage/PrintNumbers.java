b821b8d9da6ca1b7b634be19cf96e8f78d9d2e84
Ratnakumar <Ratnakumar@WSHYD-021.WILCO.COM>
2020-02-28 15:33:43 +0530

    updated methods

--- SamplePrograms/src/myPackage/PrintNumbers.java
+++ SamplePrograms/src/myPackage/PrintNumbers.java
@@ -4,15 +4,23 @@ public class PrintNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		for (int i=0; i<=100; i++)
-		{
-			if(i%5==0)
-			{
-			System.out.println("the  numbers are "+ i);
-			}
-		}
+
+		PrintNumbers print = new PrintNumbers();
+		print.printNumbers();
 
 	}
 
+	public void printNumbers()
+	{
+		for (int i=0; i<=10; i++)
+		{
+			if(i%3==0)
+			{
+				System.out.println("the  numbers are "+ i);
+				
+			}
+		}
+
+
+	}
 }
