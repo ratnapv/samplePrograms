33e6bbc3e4781526131c78eab5606516879cdb06
Ratnakumar <Ratnakumar@host.docker.internal>
2020-03-05 17:13:49 +0530

    Done

--- SamplePrograms/src/Selenium/TakeScreenshots.java
+++ SamplePrograms/src/Selenium/TakeScreenshots.java
@@ -1,18 +1,29 @@
 package Selenium;
 
+import java.io.File;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 
 public class TakeScreenshots {
 
 	public static void main(String[] args) throws Exception {
-		
-		WebDriver driver;
-		System.setProperty("webdriver.firefox.marionette","C:\\Users\\Ratnakumar\\Downloads\\Selenium jar downloads\\geckodriver.exe");
-		driver = new FirefoxDriver();
+
+		//driver;
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Ratnakumar\\Downloads\\Selenium jar downloads\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
-		Thread.sleep(50000);
+		Thread.sleep(5000);
 		System.out.println("Test");
+
+		TakesScreenshot scrShot = ((TakesScreenshot) driver);
+		File  Source = scrShot.getScreenshotAs(OutputType.FILE);
+		File Destination = new File("F:\\Screenshots\\test.png");
+		FileHandler.copy(Source, Destination);
+		driver.close();
+
 	}
 
 }
