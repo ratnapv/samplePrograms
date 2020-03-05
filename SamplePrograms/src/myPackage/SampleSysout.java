41d43d8c82358d629fa1833970fd15273f1a35fb
himesh <himesh@WSHYD-045.WILCO.COM>
2020-03-05 15:36:24 +0530

    Class modified

--- SamplePrograms/src/myPackage/SampleSysout.java
+++ SamplePrograms/src/myPackage/SampleSysout.java
@@ -3,7 +3,7 @@ package myPackage;
 public class SampleSysout {
 
 	public static void main(String[] args) {
-		System.out.println("My Name is KSK Himesh");
+		System.out.println("My Name is K S K Himesh");
 
 	}
 
